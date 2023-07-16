80f53945740b29e4f0b937b1d621899a8c767795
Kenkarate <kenkarateindia@gmail.com>
2023-07-16 20:08:46 +0530

    logo added

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,10 @@ import { DownOutlined, SmileOutlined } from "@ant-design/icons";
 import { Dropdown, Space } from "antd";
 import Link from "next/link";
 import React from "react";
+import logo from "@/public/NexusCraft LOGO/1.png";
+import Image from "next/image";
 
-function Header({color,textColor}) {
+function Header({ color, textColor }) {
   const items = [
     {
       key: "1",
@@ -20,9 +22,14 @@ function Header({color,textColor}) {
     },
   ];
   return (
-    <div class="w-full h-[7vh] flex justify-between select-none text-blueish " style={{backgroundColor:color,color:textColor}}>
+    <div
+      class="w-full h-[7vh] flex justify-between select-none text-blueish "
+      style={{ backgroundColor: color, color: textColor }}
+    >
       <div className="pl-[12vw] my-auto text-lg font-semibold cursor-pointer hidden md:block">
-        <Link href={"/"}>Home</Link>
+        <Link href={"/"}>
+          <Image style={{ width: "60px" }} src={logo} />
+        </Link>
       </div>
       <div className="px-[20vw] my-auto flex gap-5 text-lg font-semibold cursor-pointer">
         <Link href={"/"}>Home</Link>
